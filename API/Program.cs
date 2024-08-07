0f068605ce9a083bb04dc104ffb579920cbf75fd
Rameshkumar <ramesh0505rk@gmail.com>
2024-08-07 19:25:32 +0530

    Client side added

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'client';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    })
+  }
+}
