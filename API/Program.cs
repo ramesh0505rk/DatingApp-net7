e1457c4049d6db2d2f4f03d13a58e07a3ac92afe
Rameshkumar <ramesh0505rk@gmail.com>
2024-08-07 12:49:13 +0530

    Re-add API directory

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Security.Cryptography.Xml;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt=>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
+//hello
